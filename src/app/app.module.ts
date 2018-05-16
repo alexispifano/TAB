82a0bb7dc55feaca2e061d35cb666f2b580fa894
Alexis Pífano <alexis.pifano@ESP0040.local>
2018-05-16 17:40:59 +0200

    Componente fecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FechaactualComponent } from './fechaactual/fechaactual.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FechaactualComponent
   ],
   imports: [
     BrowserModule,
